3b452e6bcc265e6fa8325c959294ba3b5a1f1dbf
Ralph Sutherland <sutherland.ralph@gmx.com>
2022-09-18 16:40:02 +1000

    fixed typo in test scripts: Map51 -> map51 for script exe var for case sensitive systems

--- lab/scripts/MV51tests/runtests.sh
+++ lab/scripts/MV51tests/runtests.sh
@@ -6,7 +6,7 @@ set d=`(date "+%s" | awk '{print substr(sprintf("%X",$0),3,6)}')`
 set r='MV51Results'
 set s='MV51Scripts'
 set i="MV51Inputs"
-set exe="Map51"
+set exe="map51"
 #
 echo " MV 5.1 Tests: Clearing Area ..."
 cp "data/PHOTDAT.txt" "$i/PHOTDAT_prev.txt"
