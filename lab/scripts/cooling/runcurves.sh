8785b0bbeb26b91a4cc7ac5c4bc9a8089a34446b
Prof Ralph Sutherland <sutherland.ralph@gmx.com>
2024-09-22 16:43:12 +1000

    Create runcurves.sh
    adding missing files from GitHub transfer.  This small script but all the line data was missing too. will add those as soon as I can get the GH client app to recognise the data/lines change.

---
+++ lab/scripts/cooling/runcurves.sh
@@ -0,0 +1,31 @@
+#!/bin/tcsh
+# v5.2.0
+# tcsh for timing format and $HOST (instead of $HOSTNAME for bash)
+#
+set d=`(date "+%s" | awk '{print substr(sprintf("%X",$0),3,6)}')`
+set exe='map50'
+#
+echo " MV 5.2.0 Curve Tests: Clearing Area ..."
+rm -f timing.txt map5output.txt
+rm -f *.csv *.neq *.cie *.pie
+echo " MV 5.2.0 Test 1: NEQ cooling 3 < logT < 9 ..."
+date | awk '{print " "$0}' > timing.txt
+echo " $HOST" >> timing.txt
+(time ./$exe < neq_sol.mv >map5output.txt)| awk '{print " "$1" "$2" "$3" "$4}'>> timing.txt
+cat timing.txt >> "MVCtest_$d.txt"
+cat timing.txt
+echo " MV 5.2.0 Test 2: PIE 3 < logQH < 15 ..."
+date | awk '{print " "$0}' > timing.txt
+echo " $HOST" >> timing.txt
+(time ./$exe < pie_sol.mv >>map5output.txt)| awk '{print " "$1" "$2" "$3" "$4}'>> timing.txt
+cat timing.txt >> "MVCtest_$d.txt"
+cat timing.txt
+echo " MV 5.2.0 Test 3: CIE 2 < logT < 9 ..."
+date | awk '{print " "$0}' > timing.txt
+echo " $HOST" >> timing.txt
+(time ./$exe < cie_sol.mv >>map5output.txt)| awk '{print " "$1" "$2" "$3" "$4}'>> timing.txt
+cat timing.txt >> "MVCtest_$d.txt"
+cat timing.txt
+echo " MV 5.2.0 Curve Tests completed."
+# rm -f timing.txt map5output.txt
+# rm -f *.csv *.neq *.cie *.pie
