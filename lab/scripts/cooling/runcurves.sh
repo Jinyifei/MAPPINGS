8bc2ff6f34f28342705e5fc7f5eb3bc20bdc460b
Ralph Sutherland <sutherland.ralph@gmx.com>
2025-01-07 01:09:58 +1100

    fixed run curves lab/scripts/cooling for bash from tcsh for modern linuxs that dont install tcsh by default - rss

--- lab/scripts/cooling/runcurves.sh
+++ lab/scripts/cooling/runcurves.sh
@@ -1,31 +1,31 @@
-#!/bin/tcsh
-# v5.2.0
-# tcsh for timing format and $HOST (instead of $HOSTNAME for bash)
+#!/bin/bash
+# v5.2.1
 #
-set d=`(date "+%s" | awk '{print substr(sprintf("%X",$0),3,6)}')`
-set exe='map50'
+TIMEFORMAT="%R real %U user %S sys %P cpu%%"
+d=`(date "+%s" | awk '{print substr(sprintf("%X",$0),3,6)}')`
+exe="$HOME/mappings520/bin/map52"
 #
-echo " MV 5.2.0 Curve Tests: Clearing Area ..."
+echo " MV 5.2.1 Curve Tests: Clearing Area ..."
 rm -f timing.txt map5output.txt
 rm -f *.csv *.neq *.cie *.pie
-echo " MV 5.2.0 Test 1: NEQ cooling 3 < logT < 9 ..."
+echo " MV 5.2.1 Test 1: NEQ cooling 3 < logT < 9 ..."
 date | awk '{print " "$0}' > timing.txt
-echo " $HOST" >> timing.txt
-(time ./$exe < neq_sol.mv >map5output.txt)| awk '{print " "$1" "$2" "$3" "$4}'>> timing.txt
+echo " $HOSTNAME" >> timing.txt
+(time $exe < neq_sol.mv >map5output.txt)| awk '{print " "$1" "$2" "$3" "$4}'>> timing.txt
 cat timing.txt >> "MVCtest_$d.txt"
 cat timing.txt
-echo " MV 5.2.0 Test 2: PIE 3 < logQH < 15 ..."
+echo " MV 5.2.1 Test 2: PIE 3 < logQH < 15 ..."
 date | awk '{print " "$0}' > timing.txt
-echo " $HOST" >> timing.txt
-(time ./$exe < pie_sol.mv >>map5output.txt)| awk '{print " "$1" "$2" "$3" "$4}'>> timing.txt
+echo " $HOSTNAME" >> timing.txt
+(time  $exe < pie_sol.mv >>map5output.txt)| awk '{print " "$1" "$2" "$3" "$4}'>> timing.txt
 cat timing.txt >> "MVCtest_$d.txt"
 cat timing.txt
-echo " MV 5.2.0 Test 3: CIE 2 < logT < 9 ..."
+echo " MV 5.2.1 Test 3: CIE 2 < logT < 9 ..."
 date | awk '{print " "$0}' > timing.txt
-echo " $HOST" >> timing.txt
-(time ./$exe < cie_sol.mv >>map5output.txt)| awk '{print " "$1" "$2" "$3" "$4}'>> timing.txt
+echo " $HOSTNAME" >> timing.txt
+(time  $exe < cie_sol.mv >>map5output.txt)| awk '{print " "$1" "$2" "$3" "$4}'>> timing.txt
 cat timing.txt >> "MVCtest_$d.txt"
 cat timing.txt
-echo " MV 5.2.0 Curve Tests completed."
-# rm -f timing.txt map5output.txt
-# rm -f *.csv *.neq *.cie *.pie
+echo " MV 5.2.1 Curve Tests completed."
+rm -f timing.txt map5output.txt
+rm -f *.csv *.neq *.cie *.pie
